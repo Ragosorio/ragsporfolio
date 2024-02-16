8661242990bb5f8303679d329a251d8fafebaadb
Rolando Osorio <ragosorio777@gmail.com>
2024-02-16 11:18:59 -0600

    Connect vercel antys

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
